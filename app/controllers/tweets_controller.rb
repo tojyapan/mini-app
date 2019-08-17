9f207d2b7d3a16b60d67de0fa0ca177af1f2f8bc
Toya Kawakami <tojyapan925@gmail.com>
2019-08-17 13:10:01 +0900

    complete mini-app

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,57 @@
 class TweetsController < ApplicationController
-  def index
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
+  def index
+    set_tweets
+  end
+
+  def new
+    @tweet = Tweet.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    set_tweets
+    respond_tweet
+  end
+
+  def edit
+    respond_tweet
+  end
+
+  def update
+    @tweet.update(tweet_params)
+    set_tweets
+    respond_tweet
+  end
+
+  def destroy
+    @tweet.destroy
+    set_tweets
+    respond_tweet
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def set_tweets
+    @tweets = Tweet.order("created_at DESC").includes(:user).limit(20)
+  end
+
+  def respond_tweet
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :tweets
   root 'tweets#index'
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
